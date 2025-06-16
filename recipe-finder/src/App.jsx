477b897d1b7912b5803c1b49e085e3efb5df3846
Manjula.V <manjulavenkatappa57@gmail.com>
2025-06-16 11:44:20 +0530

    files are modified

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -6,7 +6,23 @@ function App() {
   const [query, setQuery] = useState("");
   const [reciepe, setReciepe] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [favorites, setFavorites] = useState(() => {
+    const saved = localStorage.getItem("favorites");
+    return saved ? JSON.parse(saved) : [];
+  });
+  const addToFavorite = (reciepe) => {
+    if (!favorites.find((fav) => fav.idMeal === reciepe.idMeal)) {
+      const updated = [...favorites, reciepe];
+      setFavorites(updated);
+      localStorage.setItem("favorites", JSON.stringify(updated));
+    }
+  };
 
+  const removeFromFavorite = (reciepe) => {
+    const updated = favorites.filter((fav) => fav.idMeal === reciepe.idMeal);
+    setFavorites(updated);
+    localStorage.setItem("favorites", JSON.stringify(updated));
+  };
   const searchReciepe = async () => {
     setLoading(true);
     try {
@@ -42,18 +58,49 @@ function App() {
         <p>No reciepe found</p>
       ) : (
         <div>
-          {reciepe.map((meal) => (
-            <div key={meal.idMeal}>
-              <h1>{meal.strMeal}</h1>
-              <img src={meal.strMealThumb} alt={meal.strMeal} width={200} />
-              <p>
-                {meal.strArea}| {meal.strCategory}
-              </p>
-              <a href={meal.strYoutube} target="_blank" rel="noreferrer">
-                Watch Video
-              </a>
-            </div>
-          ))}
+          {reciepe.map((meal) => {
+            const isFavorites = favorites.some(
+              (fav) => fav.idMeal === meal.idMeal
+            );
+            return (
+              <div key={meal.idMeal}>
+                <h1>{meal.strMeal}</h1>
+                <img src={meal.strMealThumb} alt={meal.strMeal} width={200} />
+                <p>
+                  {meal.strArea}| {meal.strCategory}
+                </p>
+                <a href={meal.strYoutube} target="_blank" rel="noreferrer">
+                  Watch Video
+                </a>
+                {isFavorites ? (
+                  <button
+                    onClick={() => removeFromFavorite(meal.idMeal)}
+                    className="bg-orange-400 text-white px-4 py-2 rounded"
+                  >
+                    💔 Remove Favorite
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => addToFavorite(meal)}
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                  >
+                    ❤️ Save Favorite
+                  </button>
+                )}
+              </div>
+            );
+          })}
+          <h2>Your Favorites</h2>
+          <div className="favorite-list">
+            {favorites.map((meal) => (
+              <div key={meal.idMeal}>
+                <p>{meal.strMeal}</p>
+                <button onClick={() => removeFromFavorite(meal.idMeal)}>
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
